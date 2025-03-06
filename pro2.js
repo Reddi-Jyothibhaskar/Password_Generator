339061db11bfdc30fc749a1464a9c5c561db591d
Reddi-Jyothibhaskar <reddijyothibhaskar@gmail.com>
2025-03-07 00:41:42 +0530

    Add files via upload

---
+++ pro2.js
@@ -0,0 +1,21 @@
+function generatePassword()
+{
+	let length = parseInt(document.getElementById("length").value);	
+	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";	
+	let password = "";
+	if(isNaN(length) || length < 4 || length > 32)
+	{
+		document.getElementById("password").textContent = "Please enter a valid length (4-32).";
+		return;
+	}
+	for(let i = 0;i < length; i++)
+	{
+		let randomIndex = Math.floor(Math.random()*chars.length);
+		password += chars[randomIndex];
+		if(i < length - 1)
+		{
+			password += ' ';
+		}
+	}
+	document.getElementById("password").textContent = password;
+}
